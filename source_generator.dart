d3e9dc84800d9d72b9300f916dd92012aabc5670
HYDRAoo7 <64904241+vedant6565@users.noreply.github.com>
2025-02-15 09:06:24 +0530

    Update source_generator.dart

--- source_generator.dart
+++ source_generator.dart
@@ -80,7 +80,7 @@ List<Source> _searchJsSources(Directory dir) {
                 ..appMinVerReq =
                     sourceJson["appMinVerReq"] ?? defaultSource.appMinVerReq
                 ..sourceCodeUrl =
-                    "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/$branchName/javascript/${sourceJson["pkgPath"] ?? sourceJson["pkgName"]}";
+                    "https://raw.githubusercontent.com/vedant6565/mangayomi-extensions/$branchName/javascript/${sourceJson["pkgPath"] ?? sourceJson["pkgName"]}";
               if (sourceJson["id"] != null) {
                 source = source..id = int.tryParse("${sourceJson["id"]}");
               }
